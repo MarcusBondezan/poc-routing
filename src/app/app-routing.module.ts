03048ebad644d2dd36de9ff7557e9167acef1e28
Marcus V N Bondezan <bondezan.palestart@outlook.com>
2019-08-06 23:07:23 -0300

    Updating angular to v8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', canActivate: [AuthGuard], loadChildren: './public/public.module#PublicModule' },
-  { path: 'dashboard', loadChildren: './protected/protected.module#ProtectedModule' }
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule)
+  }
   //https://stackoverflow.com/questions/55365464/same-url-path-but-loading-different-components-based-on-user-role-angular
   //https://stackoverflow.com/questions/41928727/angular-2-different-components-with-same-route?rq=1
   //https://stackoverflow.com/questions/53378649/lazy-load-modules-on-same-path-based-on-role
